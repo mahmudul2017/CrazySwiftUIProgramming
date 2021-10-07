f421423f59f51fd8df292dba1378dceb6aab2942
Mahmudul2017 <mahmudulhasanshah@gmail.com>
2021-10-07 14:57:56 +0600

    Initial Commit

---
+++ CrazySwiftUIProgramming/CrazySwiftUIProgrammingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrazySwiftUIProgrammingApp.swift
+//  CrazySwiftUIProgramming
+//
+//  Created by Jaman on 7/10/21.
+//
+
+import SwiftUI
+
+@main
+struct CrazySwiftUIProgrammingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
